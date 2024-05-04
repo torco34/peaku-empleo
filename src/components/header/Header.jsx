62e4ea5b28f80054f62f7502f1f3ee4b360bd852
Angel <angel.peraza@peaku.io>
2024-05-04 10:52:24 -0500

    feat: :art: footer acomodado, falta la interactividad y listas desplegables

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,40 +16,66 @@ export const Header = ({ navbarItems }) => {
 
   return (
     <>
-      <Navbar className="header-navbar navbar navbar-expand-lg bg-body-tertiary nav-body">
-        <Container>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="collapse navbar-collapse d-flex justify-content-end">
-            <ul className=" navbar-nav p-2 mb-2 mb-lg-0">
-              {navbarItems.map((nav, index) => (
-                <li className="nav-item" key={index}>
-                  <NavLink
-                    exact
-                    to={nav.route}
-                    className="nav-link"
-                    activeClassName="active"
-                  >
-                    <span className="mx-2 text-dark">
-                      {nav.name} {nav.icon}
-                    </span>
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-            <button >
-              <BsGlobe2 className="m-2" />
-              Español
-            </button>
-            <button >
-              <IoNotifications />              
-            </button>
-            <button >
-              <MdAccountCircle />              
-            </button>          
-                      
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary navbar1">
+      <div className="container-fluid ">
+        <a href="#"><img src="/logo-blue.png" alt="/img" /></a>
+        <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav  mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a href="" className="nav-link active" aria-current="page">Empleos</a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">Bootcamp</a>
+            </li>
+            <li class="nav-item">
+              <a href="" class="nav-link">Sobre nosotros</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Para profesionales
+              </a>
+              <ul class="dropdown-menu">
+                <li><a className="dropdown-item">Inicio</a></li>
+                <li><a className="dropdown-item">Empleos</a></li>
+                <li><a className="dropdown-item">Cursos y retos</a></li>
+                <li><a className="dropdown-item">Preguntas</a></li>
+                <li><a className="dropdown-item">Profesores</a></li>
+                <li><a className="dropdown-item">Bootcamp</a></li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Para empresas
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item">Inicio</a></li>
+                <li><a className="dropdown-item">Publicar empleo</a></li>
+                <li><a className="dropdown-item">Nuestro proceso</a></li>
+                <li><a className="dropdown-item">Precios</a></li>
+                <li><a className="dropdown-item">Pruebas online</a></li>
+                <li><a className="dropdown-item">Nómina</a></li>
+                <li><a className="dropdown-item">Blog</a></li>
+                <li><a className="dropdown-item">Comercial</a></li>
+                <li><a className="dropdown-item">Calculadora de salario</a></li>
+              </ul>
+            </li>
+          </ul> 
+          
+          <button className="btn btn-outline-secondary" type="button">
+           <BsGlobe2 />
+           Español
+          </button>
+          <button className="btn btn-outline-secondary" type="button">
+           <IoNotifications />
+          </button>
+          <button className="btn btn-outline-secondary ">
+           <MdAccountCircle className="" />
+          </button>
+        </div>
+      </div>
+    </nav>
+
+
       <div className="bg-primary container-fluid navbar-expand-lg" id="containerBlue">
         <div className="row">
           <div className="col">
@@ -71,6 +97,7 @@ export const Header = ({ navbarItems }) => {
         </div>
       </div>
       <MenuVertical />
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
       {/* AQUÍ SE  RENDERIZAN  LAS PAGINAS EN <Outlet/> */}
       <Outlet />
